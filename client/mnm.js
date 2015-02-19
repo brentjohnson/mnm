1bf32a19d9370d98ea00a632cb2a296f2e3f3241
Brent Johnson <BrJohnson@VSCAT.com>
2015-02-18 21:52:31 -0500

    Build a deck

--- client/mnm.js
+++ client/mnm.js
@@ -3,5 +3,20 @@ Template.body.helpers({
 	 cardpool: function () {
         // Otherwise, return all of the tasks
         return CardCollection.find({user: Meteor.userId()}, {sort: {name: 1, id: 1}});
+    },
+    decklist: function () {
+    	return DeckCollection.find({user: Meteor.userId()}, {sort: {name: 1, id: 1}});
     }
+});
+
+Template.body.events({
+    "click #cardcollection .result-row": function () {
+      // Set the checked property to the opposite of its current value
+      DeckCollection.insert(this);
+    },
+    "click #deck .result-row": function () {
+      // Set the checked property to the opposite of its current value
+      DeckCollection.remove(this._id);
+    }
+
 });
