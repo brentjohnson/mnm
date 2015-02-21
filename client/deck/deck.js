20d784d24ae2edc2570afe190715215ac055d2e2
Brent Johnson <BrJohnson@VSCAT.com>
2015-02-20 21:05:17 -0500

    Fix cross origin for images.  Show a card.

--- client/deck/deck.js
+++ client/deck/deck.js
@@ -1,7 +1,6 @@
 Template.deck.helpers({
 
-	 cardpool: function () {
-        // Otherwise, return all of the tasks
+	cardpool: function () {
         return CardCollection.find({user: Meteor.userId()}, {sort: {name: 1, id: 1}});
     },
     decklist: function () {
