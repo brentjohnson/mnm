7c70295642c679ea5ce637b0b1237b01ab9387b4
Brent Johnson <BrJohnson@VSCAT.com>
2015-02-22 10:39:03 -0500

    Cleaned up router.  Fixed /public.

--- client/router.js
+++ client/router.js
@@ -12,37 +12,21 @@ Router.onBeforeAction(function () {
   }
 });
 
-/*
-Router.route('/', function () {
-  this.render('deck');
-});
-*/
-Router.route('/', function () {
-  if (!Meteor.userId()) {
-    // if the user is not logged in, render the Login template
-    this.render('landing');
-  } else {
-    // otherwise don't hold up the rest of hooks or our route/action function
-    // from running
-    this.redirect('/deck');
-  }
-});
-Router.route('/landing');
-Router.route('/deck');
-Router.route('/game');
+Router.map(function(){
+  
+  this.route('/', function () {
+    if (!Meteor.userId()) {
+      // if the user is not logged in, render the Login template
+      this.render('landing');
+    } else {
+      // otherwise don't hold up the rest of hooks or our route/action function
+      // from running
+      this.redirect('/deck');
+    }
+  });
 
-/*Router.map(function() {
+  this.route('/landing');
+  this.route('/deck');
+  this.route('/game');
 
-
-	this.route('index', {
-		path: '/',
-		action: function() {
-			if (!Meteor.userId()) {
-				this.render('landing');
-			} else {
-				this.render();
-			}
-		}
-	})
 });
-*/
