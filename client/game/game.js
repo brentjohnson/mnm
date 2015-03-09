e80fd9bfe7e73fdabd73432ab67d5714d781d78a
Brent Johnson <BrJohnson@VSCAT.com>
2015-03-08 21:44:51 -0400

    A (very) little formatting.

--- client/game/game.js
+++ client/game/game.js
@@ -172,7 +172,7 @@ MNM = (function() {
 
       scene = new THREE.Scene();
 
-      camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+      camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
 
       renderer = new THREE.WebGLRenderer();
       renderer.setSize(window.innerWidth, window.innerHeight);
--- server/methods.js
+++ server/methods.js
@@ -9,24 +9,27 @@ Meteor.methods({
   	}
   	// passed a leagueId
   	check(leagueId, String);
-	// league exists
-	league = Leagues.find(leagueId).fetch()[0]; 
 
-	if (!league) {
-		throw new Meteor.Error("not-found", "League ID not found.");
-	} 	
+    // league exists
+    league = Leagues.find(leagueId).fetch()[0];
+
+    if (!league) {
+      throw new Meteor.Error("not-found", "League ID not found.");
+    }
+
   	// not a member of the league
   	if (_.indexOf(league.players, Meteor.userId()) !== -1) {
-		throw new Meteor.Error("already-a-member", "Current user is already a member of this league.");
+      throw new Meteor.Error("already-a-member", "Current user is already a member of this league.");
   	}
+
   	// league not full
   	if (league.players.length >= 8) {
-		throw new Meteor.Error("league-full", "League is full.");
+      throw new Meteor.Error("league-full", "League is full.");
   	}
 
   	// add to league
   	league.players.push(Meteor.userId());
-	Leagues.update(league._id, {$set: {players: league.players}});
+    Leagues.update(league._id, {$set: {players: league.players}});
 
   	// assign cards
   	assignCards(Meteor.userId(), leagueId);
